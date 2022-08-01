c70cc8613ae5a1e7b415b60c80c743ffcc2156c8
Maria Redman <mariaredman@Marias-MacBook-Air-2.local>
2022-08-01 17:04:22 -0400

    Css starts

--- students.js
+++ students.js
@@ -6,7 +6,21 @@ const port = 4001
 app.use (cors())
 app.use(express.json())
 
+let student = ["bob", "samantha","lily", "grace", "jim"]
+
+app.get("/", (req, res) => {
+
+    let output = "<html><body><ul>" 
+    //output = output + " hello there html"
+    for(let i = 0; i < students.length; i++){
+        output = output + "<li><input type="checkbox">" + 
+        students [i] + "</li>"
+}
+output = output + "</ul></body></html>"
+    res.send(output)
+})
+
 app.listen(port, () => {
-console.log("I was needed")
+console.log("localhost 4001")
 
 })
